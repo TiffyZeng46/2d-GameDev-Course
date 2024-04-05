bc14d115171a0a4e3fd36cc166aa05944b5c6f32
TiffyZeng46 <tizeng@s.sfusd.edu>
2024-04-05 15:43:11 -0700

    L5 Code Player Health and Practice Completed

--- 05.loot_it_all_workbook/practices/L5.P1.code_an_energy_pack/ship_energy_pack.gd
+++ 05.loot_it_all_workbook/practices/L5.P1.code_an_energy_pack/ship_energy_pack.gd
@@ -8,7 +8,10 @@ var energy := 20.0
 
 
 func _ready() -> void:
-	get_node("UI/EnergyBar").value = energy
+	area_entered.connect(_on_area_entered)
+	
+	
+	set_energy(energy)
 
 
 func _process(delta: float) -> void:
@@ -23,5 +26,12 @@ func _process(delta: float) -> void:
 		get_node("Sprite2D").rotation = velocity.angle()
 
 
-func _on_area_entered(area: Area2D) -> void:
-	pass
+func set_energy(new_energy: int) -> void:
+	energy = new_energy
+	get_node("UI/EnergyBar").value = energy
+	
+func _on_area_entered(_area_that_entered: Area2D) -> void:
+	set_energy(energy + 20)
+	
+	
+
