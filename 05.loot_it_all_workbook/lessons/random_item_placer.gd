e3fedb7d38645be2c45dca21e07de85c05028ae3
TiffyZeng46 <tizeng@s.sfusd.edu>
2024-04-16 13:52:21 -0700

    L7 Spawn Items Randomly and Practices Complete

--- 05.loot_it_all_workbook/lessons/random_item_placer.gd
+++ 05.loot_it_all_workbook/lessons/random_item_placer.gd
@@ -1,21 +1,20 @@
 extends Node2D
 
-var collectible_scenes := [
-	preload("res://practices/L7.P1.spawn_random_items/energy_pack.tscn"),
-	preload("res://practices/L7.P1.spawn_random_items/coin.tscn")
+var item_scenes := [
+	preload("res://lessons/gem.tscn"),
+	preload("res://lessons/health_pack.tscn")
 ]
-
 
 func _ready() -> void:
 	get_node("Timer").timeout.connect(_on_timer_timeout)
 
 func _on_timer_timeout() -> void:
-	var random_item_scene: PackedScene= collectible_scenes.pick_random()
+	var random_item_scene: PackedScene= item_scenes.pick_random()
 	var item_instance := random_item_scene.instantiate()
 	add_child(item_instance)
 	
 	var viewport_size := get_viewport_rect().size
 	var random_position := Vector2(0, 0)
-	
+	random_position.x = randf_range(0, viewport_size.x)
+	random_position.y = randf_range(0, viewport_size.y)
 	item_instance.position = random_position
-
